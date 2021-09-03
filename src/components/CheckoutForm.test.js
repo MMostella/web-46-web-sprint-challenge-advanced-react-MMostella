d90cbf0912db37b16353156872c728d45e7d206b
MMostella <masonmostella@gmail.com>
2021-09-03 10:12:37 -0500

    Added get Inputs. added userEvents

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import MutationObserver from 'mutationobserver-shim';
 import { render } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import userEvent from "@testing-library/user-event";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
@@ -11,5 +12,19 @@ test("renders without errors", () => {
 });
 
 test("shows success message on submit with form details", () => {
-
+    render(<CheckoutForm/>);
+    const firstNameInput = screen.getByLabelText(/first name/i);
+    userEvent.type(firstNameInput, 'Mason');
+    const lastNameInput = screen.getByLabelText(/last name/i);
+    userEvent.type(lastNameInput, 'Mostella');
+    const addressInput = screen.getByLabelText(/address/i);
+    userEvent.type(addressInput, '123 address rd');
+    const cityInput = screen.getByLabelText(/city/i);
+    userEvent.type(cityInput, 'Boaz');
+    const stateInput = screen.getByLabelText(/state/i);
+    userEvent.type(stateInput, 'Alabama');
+    const zipInput = screen.getByLabelText(/zip/i);
+    userEvent.type(zipInput, 35951);
+    const button = screen.getByRole('button');
+    userEvent.click(button);
 });
